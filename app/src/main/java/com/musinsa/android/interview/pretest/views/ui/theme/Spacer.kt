2fb8bda1085622867cd9f623482df549d1881e22
kyungsoo-dev <kyungsoo7135@gmail.com>
2025-03-25 11:14:15 +0900

    feat: add common Spacer composables

---
+++ app/src/main/java/com/musinsa/android/interview/pretest/views/ui/theme/Spacer.kt
@@ -0,0 +1,22 @@
+package com.musinsa.android.interview.pretest.views.ui.theme
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun hspace(value: Int) {
+    Spacer(
+        modifier = Modifier.width(value.dp)
+    )
+}
+
+@Composable
+fun vspace(value: Int) {
+    Spacer(
+        modifier = Modifier.height(value.dp)
+    )
+}
